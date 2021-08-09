d053a0115faf83f71aecd82c757d7f4577de192e
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2021-08-09 00:38:35 -0300

    Deleted family name and made a migration from new database to old database
    Signed-off-by: Bruno Ellerbach <ingbrunofernandez@gmail.com>

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -12,7 +12,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
-@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 3, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
     private static final String NAME_DATABASE = "agenda.db";
@@ -28,6 +28,23 @@ public abstract class AgendaDatabase extends RoomDatabase {
                         @Override
                         public void migrate(@NonNull SupportSQLiteDatabase database) {
                             database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                        }
+                    }, new Migration(2, 3) {
+                        @Override
+                        public void migrate(@NonNull SupportSQLiteDatabase database) {
+                            // create new table with informations
+                            database.execSQL("CREATE TABLE IF NOT EXISTS `Aluno_new` " +
+                                    "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                                    "`nome` TEXT, " +
+                                    "`telefone` TEXT, " +
+                                    "`email` TEXT)");
+                            //copy data from old table to new
+                            database.execSQL("INSERT INTO Aluno_new (id, nome, telefone, email)" +
+                                    "SELECT id, nome, telefone, email FROM Aluno");
+                            //delete old table
+                            database.execSQL("DROP TABLE Aluno");
+                            //Rename new table to old table name
+                            database.execSQL("ALTER TABLE Aluno_new RENAME TO Aluno");
                         }
                     })
                     .build();
