9bd819f3c7086d66ed3b8dcf474af6f42851c836
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2021-08-09 00:23:03 -0300

    Added amily name and added migration of the database
    Signed-off-by: Bruno Ellerbach <ingbrunofernandez@gmail.com>

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -2,24 +2,37 @@ package br.com.alura.agenda.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import br.com.alura.agenda.database.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
-@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
     private static final String NAME_DATABASE = "agenda.db";
+    private static AgendaDatabase instance;
 
     public static AgendaDatabase getInstance(Context context) {
-        return Room.databaseBuilder(context,
-                AgendaDatabase.class,
-                NAME_DATABASE)
-                .allowMainThreadQueries()
-                .build();
+        if (instance == null) {
+            instance = Room.databaseBuilder(context,
+                    AgendaDatabase.class,
+                    NAME_DATABASE)
+                    .allowMainThreadQueries()
+                    .addMigrations(new Migration(1, 2) {
+                        @Override
+                        public void migrate(@NonNull SupportSQLiteDatabase database) {
+                            database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                        }
+                    })
+                    .build();
+        }
+        return instance;
     }
 
     public abstract AlunoDAO getRoomAlunoDAO();
